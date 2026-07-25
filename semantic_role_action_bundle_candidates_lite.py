4e6466daeec1d78a3dda12d3f537d89f31d76d2b
Hikmet Pınarbaş <108670827+Hikmetpinarbas@users.noreply.github.com>
2026-07-25 23:00:28 +0300

    add semantic role action bundle CLI

---
+++ semantic_role_action_bundle_candidates_lite.py
@@ -0,0 +1,5 @@
+from hpfa.modules.core.semantic_role_action_bundle_candidates_lite.src.semantic_role_action_bundle_candidates import main
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
